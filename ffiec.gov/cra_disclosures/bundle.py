a29cc2e7b9cc8f82071042b26cf787b8645410d5
Eric Busboom <eric@clarinova.com>
2014-06-23 18:24:22 -0700

    .

--- ffiec.gov/cra_disclosures/bundle.py
+++ ffiec.gov/cra_disclosures/bundle.py
@@ -16,6 +16,7 @@ class Bundle(BuildBundle):
     def meta_generate_dd_rows(self, year):
         '''Read the file and combines multiple comment rows into single field entry rows'''
         import csv
+        import re
         
         fn = self.filesystem.path('meta','disclosure_datadicts','datadict-disclosures-{}.csv'.format(year))
         
@@ -45,7 +46,9 @@ class Bundle(BuildBundle):
                 else:
                     out_row['Comments'] += ( '\n' + r['Comments'])
                     out_row['Field'] +=  ( ' ' + ' '.join([ x.strip() for x in r['Field'].split()]))
-        
+                    out_row['Field'] = re.sub(r'\(Originations (?:and|\&) Purchases\)', '', out_row['Field'])
+                    out_row['Field'] = out_row['Field'].strip('.')
+                    
             yield out_row
         
     def meta_generate_tables(self, year):
@@ -57,6 +60,7 @@ class Bundle(BuildBundle):
             if row['Start'] == '1': # The start of a table. 
                 if table:
                     yield table_name, table
+                    table = []
             
                 table_name = row['Comments'].strip().split(' ')[-1].lower().replace('-','_')
                 
@@ -66,39 +70,97 @@ class Bundle(BuildBundle):
 
         yield table_name, table
         
+    def make_column_name(self, field):
+    
+    
+        x = []
+        
+        field = field.lower()
+
+        if field.startswith('number'):
+            x.append('num')
+        elif field.startswith('total'):
+            x.append('tot')
+        else:
+            return x
+            
+        if  'originated' in field:
+            x.append('orig')
+        elif 'purchased' in field:
+            x.append('purch')
+        
+        if 'business' in field:
+            x.append('bus')
+        elif 'farm' in field:
+            x.append('farm')   
+        
+        if 'affiliate' in field:
+            x.append('al')
+        
+        if '> $100,000' in field:
+            x.append('gt100k')
+            
+        if '> $250,000' in field:
+            x.append('gt250k')
+            
+        if '> $500,000' in field:
+            x.append('gt500k')
+        
+        if '< $100,000' in field:
+            x.append('lt100k')
+            
+        if '< $250,000' in field:
+            x.append('lt250k')
+            
+        if '< $500,000' in field:
+            x.append('lt500k')
+        
+        if '< $1 million' in field or '< $1,000,000' in field:
+            x.append('lt1m')
+
+            
+        return x
+    
 
     def meta(self):
-        import re, collections
-        
-        
+        import re
+
         tables = {}
         try:
             field_names = self.filesystem.read_yaml('meta','field_map.yaml')
-        except:
-            raise 
+        except IOError:
+            field_names = None
             
         if not field_names:
             field_names = {}
 
-        tables = colections.defaultdict(lambda: defaultdict(list))
+        tables = dict()
 
         for year in range(1996, 2013):
             for table_name, table in self.meta_generate_tables(year):
-                #print '------- {} {} ---------'.format(year, table_name)
+                print '------- {} {} ---------'.format(year, table_name)
+
                 for i,row in enumerate(table):
                     #print row['Start'], row['End'], row['Field']
                     fn = re.sub(r'^\d+[\.\s]*','',row['Field'].strip())
                     if not fn in field_names:
-                        field_names[fn] = ''
+                        field_names[fn] = '_'.join(self.make_column_name(fn))
                         
-                    tables[table_name][i].append(fn)
+                    # default dict cant be stored in Yaml with out special converter class
+                    if not table_name in tables:
+                        tables[table_name] = {}
+                        
+                    if i not in tables[table_name]:
+                        tables[table_name][i] = []
+              
+                    if fn.lower() not in tables[table_name][i]:
+                        tables[table_name][i].append(fn.lower())
 
-                    
+                     
+
         self.filesystem.write_yaml(field_names, 'meta','field_map.yaml')
                     
-
-        print tables
-
+        self.filesystem.write_yaml(tables, 'meta','column_names_by_pos.yaml')
 
     def build(self):
         import uuid
