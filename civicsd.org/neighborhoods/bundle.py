4395e0b2cacdd0a4d28a3fb576ada357844fa1a6
Eric Busboom <eric@clarinova.com>
2014-09-24 21:38:55 -0700

    .

--- civicsd.org/neighborhoods/bundle.py
+++ civicsd.org/neighborhoods/bundle.py
@@ -2,36 +2,47 @@ from  ambry.bundle.geo import GeoBuildBundle
 
 class Bundle(GeoBuildBundle):
     ''' '''
-    
-                 
+        
+    def build(self):
+        
+        r = super(Bundle,self).build()
+        
+        if r:
+            self.build_block_cross()
+            
+        return r
+        
+            
     def build_block_cross(self):
         """Build the bus_block_cross crosswalk file to assign businessed to blocks. """
         from ambry.geo.util import find_geo_containment
 
         def generate_geometries():
-            
+            """The Containing geometries are the neighborhoods. """
+
+            for row in self.partitions.find(table = 'neighborhoods').query(
+                    "SELECT AsText(geometry) as wkt, objectid_1 as id FROM neighborhoods"):
+
+                yield row['id'], row['id'],  row['wkt']
+
+        def generate_points():
+            """The points we are going to find the containment of are the centroids of the census blocks"""
             blocks = self.library.dep('blocks').partition
             lr = self.init_log_rate(3000)
             
             # Note, ogc_fid is the primary key. The id column is created by the shapefile. 
-            for i,block in enumerate(blocks.query("SELECT  AsText(geometry) AS wkt, geoid FROM  blocks")):
+            for i,row in enumerate(blocks.query(
+                "SELECT  X(Centroid(geometry)) AS lon,  Y(Centroid(geometry)) AS lat, geoid FROM  blocks")):
                 lr('Load rtree')
                 
                 if self.run_args.test and i > 200:
                     break
                 
-                yield i, block['geoid'] , block['wkt']
-        
-        def generate_points():
-            p = self.partitions.find(table = 'dstk_addresses')
-            #p = self.library.get('sandiego.gov-businesses-orig-dstk_addresses-1.0.3').partition
-            for row in p.rows:
-                if  row['lon'] and row['lat']:
-                    yield (row['lon'], row['lat']), row['businesses_id']
+                yield  (row['lon'], row['lat']), row['geoid'] 
 
         def mark_contains():
             
-            p = self.partitions.find_or_new(table='bus_block_cross')
+            p = self.partitions.find_or_new(table='nhood_block_cross')
             p.clean()
             
             lr = self.init_log_rate(3000)
@@ -39,13 +50,8 @@ class Bundle(GeoBuildBundle):
             with p.inserter() as ins:
                 while True:
                     (p,point_obj,geometry, poly_obj) = yield # Get a value back from find_geo_containment
-                    
-                    d = dict(businesses_id = point_obj, 
-                            block_geoid = poly_obj, # New name
-                            geoid = poly_obj # Old name, for development
-                    )
 
-                    ins.insert(d)
+                    ins.insert(dict(neighborhoods_id = poly_obj, geoid = point_obj ))
                     lr('Marking point containment')
         
             
