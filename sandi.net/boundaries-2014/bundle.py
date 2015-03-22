9b5166b8262de1c2fd1f3439dc7a597908448bd2
Eric Busboom <eric@clarinova.com>
2015-03-22 13:41:12 -0700

    moved a lot of bundles to own repos

---
+++ sandi.net/boundaries-2014/bundle.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+
+from  ambry.bundle import BuildBundle
+ 
+
+
+class Bundle(BuildBundle):
+    ''' '''
+
+    def __init__(self,directory=None):
+
+        super(Bundle, self).__init__(directory)
+
+    def build(self):
+
+        return True
+
